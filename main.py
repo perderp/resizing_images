71be684c4d373b506c617d5219b03eaf95a3741f
perderp <chironjasphercainap@gmail.com>
2024-03-12 17:37:09 +0800

    update : added resize folder to subfolder and docs. bug: duplicate if run again

--- main.py
+++ main.py
@@ -43,75 +43,60 @@ moved = 0
 created_folders = []
 no_folders = len(folders)
 
-#print(f"DOCUMENTS : {docs}")
-#print(f"FOLDERS : {folders}")
-#print(path_dir_content)
 
-# go through all files and move them into according folders
-if len(folders) > 0 :
-    for folder in folders :
-        #connecting the filepath in order to create the new folder
-        subfolder_path = os.path.join(folder, 'resized_image')
+def main() :
+    if len(folders) > 0 :
+        process_folder(folders)
+
+    if len(docs) > 0:
+        process_docs(docs, folders, path)
+
+def process_folder(the_folder) :
+    if the_folder is None :
+        return False
+
+    for folder in the_folder :
+
+        new_path_content = os.listdir(folder)
+        new_path_dir_content = [os.path.join(folder, doc) for doc in new_path_content]
+
+        #filter if there's another file or folder
+        new_docs = [doc for doc in new_path_dir_content if os.path.isfile(doc)]
+        new_folder = [folder for folder in new_path_dir_content if os.path.isdir(folder)]
+
+        if len(new_folder) > 0  and "resized_image" not in new_folder:
+            process_folder(new_folder)
+
+        process_docs(new_docs, the_folder, folder)
 
 
-        folder_content = os.listdir(folder)
-        new_docs = [os.path.join(folder, doc) for doc in folder_content if os.path.isfile(os.path.join(folder, doc))]
+def process_docs(the_doc, the_folder = folders, the_path = path) :
+    subfolder_path = os.path.join(the_path, "resized_image")
 
-        for new_doc in new_docs :
-            full_path_doc, file_type = os.path.splitext(new_doc)
-            doc_path = os.path.dirname(full_path_doc)
-            doc_name = os.path.basename(full_path_doc)
-
-            if doc_name == "resized_image" or doc_name.startswith('.'):
-                continue
-
-            if subfolder_path not in folder and subfolder_path not in created_folders:
-                #create the folder here
-                try :
-                    os.mkdir(subfolder_path)
-                    created_folders.append(subfolder_path)
-                    print(f"Folder {subfolder_path} created")
-                except FileExistsError as err:
-                    print(f"Folder already exists at {subfolder_path}... {err}")
-
-            # get the new folder and copy the file
-            new_doc_path = os.path.join(subfolder_path, doc_name) + file_type
-
-            if file_type.endswith(('jpg', 'png', 'jpeg')):
-                shutil.copyfile(new_doc, new_doc_path)
-                resize_image(new_doc_path, output_filename=new_doc_path)
-            else:
-                continue
-
-if len(docs) > 0:
-    for doc in docs:
+    for doc in the_doc :
         full_path_doc, file_type = os.path.splitext(doc)
         doc_path = os.path.dirname(full_path_doc)
         doc_name = os.path.basename(full_path_doc)
 
-        # skip this file when its in the directory
+        # skip this file if when it's in the directory
         if doc_name == "resized_image" or doc_name.startswith('.'):
             continue
 
-        # get the subfolder name and create folder if not exist
-        subfolder_path = os.path.join(path, 'resized_image')
-
-        if subfolder_path not in folders and subfolder_path not in created_folders:
-            #create the folder
-            try:
+        if subfolder_path not in the_folder and subfolder_path not in created_folders and "resized_image" not in doc_name:
+            try :
                 os.mkdir(subfolder_path)
                 created_folders.append(subfolder_path)
                 print(f"Folder {subfolder_path} created")
             except FileExistsError as err:
                 print(f"Folder already exists at {subfolder_path}... {err}")
-        #get the new folder and copy the file
-        new_doc_path = os.path.join(subfolder_path, doc_name) + file_type
 
-        #print(file_type)
+        new_doc_path = os.path.join(subfolder_path, doc_name) + file_type
 
         if file_type.endswith(('jpg', 'png', 'jpeg')):
             shutil.copyfile(doc, new_doc_path)
             resize_image(new_doc_path, output_filename=new_doc_path)
-
         else:
             continue
+
+if __name__ == "__main__" :
+    main()
